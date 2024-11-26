04f8b3106dc256e3306f0f1b3b436cb95b616c85
G-DOC69 <144429432+G-DOC69@users.noreply.github.com>
2024-11-26 12:35:51 +0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_2AX2yGKHB', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6hlba5u1fsobdpaq7ng6c3kd1e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_qRu90nnjj', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'f09d3ugu4utjqgvesjq42c4qp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
